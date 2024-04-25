d387dfcbde200af6ed40712ee8026b07853665b7
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2024-04-25 17:58:22 -0300

    escrevi o enunciado da questao

---
+++ questoes slide 1/questao3.c
@@ -0,0 +1,6 @@
+/*Implemente uma algoritmo que represente a situação a seguir: Se a idade for maior ou
+igual 18 anos e idade menor ou igual 95 anos e o resultado do exame de legislação for
+APROVADO, então apresentar a mensagem de saída 'Sua carteira de motorista estará pronta
+em uma semana’. Caso contrário apresentar a mensagem de saída 'Você não possui idade
+permitida ou não passou no teste de legislação*/
+
