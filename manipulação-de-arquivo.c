7af99f5efd48aa467835b0cda9f5260d36872fc0
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2024-05-10 21:43:51 -0300

    terminei o programa de manipulaçao de arquivos

--- manipulação-de-arquivo.c
+++ manipulação-de-arquivo.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 FILE *arquivo;
 char nome[50], DataNasc[20], tamanhoDaJeba[4], linha[250], nomearquivo[30], str[80];
+int i;
 
 void LerArquivo(){
     sprintf(str, "C://temp//%s", nomearquivo);
@@ -14,6 +16,7 @@ void LerArquivo(){
 }
 
 int main(){
+    setlocale(LC_ALL,"");
     printf("Informe o nome do arquivo: Ex. Aluno.txt \n");
     gets(nomearquivo);
     sprintf(str, "..//%s", nomearquivo);
@@ -23,16 +26,20 @@ int main(){
         printf("Erro na leitura do arquivo\n");
     else
         printf("Arquivo criado com sucesso\n");
+
+for(i =1; i<=5;i++){
     printf("Informe seu nome:\n");
-    gets(nome);
+    fgets(nome, sizeof(nome), stdin);
     printf("Informe a data de nascimento:\n");
-    scanf("%s", &DataNasc);
+    fgets(DataNasc, sizeof(DataNasc), stdin);
     printf("Informe o tamanho da sua jeba:\n");
-    scanf("%s", &tamanhoDaJeba);
-        fprintf(arquivo,"Nome : %s\n", nome);
+    fgets(tamanhoDaJeba, sizeof(tamanhoDaJeba), stdin);
+    printf("Aluno Numero : %d\n", i);
+        fprintf(arquivo,"Matricula : %d\n", i);
+        fprintf(arquivo,"Nome: %s\n",nome);
         fprintf(arquivo,"Nascimento : %s\n", DataNasc);
         fprintf(arquivo,"Medida da jeba : %s\n", tamanhoDaJeba);
+}
         fclose(arquivo);
         system("pause");
-    
 }
