35a2dcbe3003c630a0258860a5b9fc93c2cd62cb
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2024-05-15 19:26:45 -0300

    consegui ajeitar a divisao por zero e a potenciaçao

--- calculadora.c
+++ calculadora.c
@@ -16,13 +16,12 @@ void MenuCalculadora (){
     printf("+=======================================+\n");
     printf("|          CALCULADORA SIMPLES          |\n");
     printf("+=======================================+\n");
-    printf("|  1-SOMAR            |   2-SUBTRAIR    |\n");
+    printf("|    1-SOMA           |   2-SUBTRACAO   |\n");
     printf("+=======================================+\n");
-    printf("|  3-MULTIPLICAR      |   4-DIVIDIR     |\n");
+    printf("|    3-MULTIPLICACAO  |   4-DIVISAO     |\n");
     printf("+=======================================+\n");
-    printf("|  5-MUDAR NUMEROS    |   6-POTENCIA    |\n");
+    printf("|    5-POTENCIACAO    |   6-SAIR        |\n");
     printf("+=======================================+\n");
-    printf("Digite a operacao desejada ou 7 para sair\n");
     scanf("%d", &operacao);
 }
 
@@ -32,7 +31,6 @@ void Entrada(){
      printf("Digite o segundo valor: \n");
      scanf("%f", &valor2);
 } 
-
 void somar(){
     res = valor1 + valor2;
 }
@@ -44,14 +42,6 @@ void multiplicar(){
 }
 void dividir(){
     res = valor1/valor2;
-    if(valor2 == 0)
-    printf("divisor nulo. Insira numero valido.");
-}
-void MudarNumeros(){
-    printf("Digite o primeiro valor: \n");
-    scanf("%f", &valor1);
-    printf("Digite o segundo valor: \n");
-    scanf("%f", &valor2);
 }
 void Potencia(){
     res = pow(valor1, valor2);
@@ -60,14 +50,12 @@ void saida(){
     printf("O resultado da operacao e: %.2lf\n", res);
 }
 int main (){
-
 do{   
     MenuCalculadora();
-
     switch(operacao)
     {
-        if(valor1 > 7 && valor1 < 0)
-        printf("insira uma tecla valido.");
+        if(valor1 > 6 && valor1 < 0)
+        printf("insira uma tecla valida.");
     case 1:
         system("cls");
         Entrada();
@@ -90,26 +78,25 @@ do{
         system("cls");
         Entrada();
         dividir();
+        if(valor2 == 0){
+        printf("Divisor nulo. Insira numero valido.\n");
+        break;}
         saida();
         break;
     case 5:
         system("cls");
         Entrada();
-        MudarNumeros();
+        Potencia();
+        saida();
         break;
     case 6:
-        system("cls");
-        Entrada();
-        Potencia();
-        break;
-    case 7:
         break;
     default:
         printf("Opcao invalida! \n");
         break;
     }
 }
-while(operacao != 5);
+while(operacao != 6 );
 }
 
 
