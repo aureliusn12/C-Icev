cc7d135b0c98c8a92971ea7d12ba290209a2e265
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2024-06-06 16:53:08 -0300

    começo da questao 3
    falta botar a parte da multiplicação

--- Assuntos p2/matriz3.c
+++ Assuntos p2/matriz3.c
@@ -3,14 +3,47 @@
 #include<stdlib.h>
 
 int i, j;
-int Dlinha, Dcoluna;
+int DlinhaA, DcolunaA, DlinhaB, DcolunaB;
 
 int main(){
-    printf("Informe a dimensao da linha: \n");
-    scanf("%d", &Dlinha);
-    printf("Informe a dimensao da coluna: \n");
-    scanf("%d", &Dcoluna);
+    printf("Informe a dimensao da linha da matriz A: \n");
+    scanf("%d", &DlinhaA);
+    printf("Informe a dimensao da coluna da matriz B: \n");
+    scanf("%d", &DcolunaA);
+    printf("Informe a dimensao da linha da matriz B: \n");
+    scanf("%d", &DlinhaB);
+    printf("Informe a dimensao da coluna da matriz B: \n");
+    scanf("%d", &DcolunaB);
+    if(DcolunaA==DlinhaB){
 
-    int matriz[i][j];
-    for(i=0; i<)
+    int matrizA [DlinhaA] [DcolunaA];
+    printf("Insira os valores da matriz A: %d x %d \n", DlinhaA, DcolunaA);
+    for(i=0; i<DlinhaA; i++){
+        for(j=0; j<DcolunaA; i++){
+            scanf("%d", &matrizA[i][j]);
+        }
+    }
+    for(i=0;i<DlinhaA; i++){
+        for(j=0; j<DcolunaA; j++){
+            printf("|%4d", matrizA[i][j]);
+        }
+        printf("|\n");
+    }
+    int matrizB [DlinhaB][ DcolunaB];
+     printf("Insira os valores da matriz B: %d x %d \n", DlinhaB, DcolunaB);
+    for(i=0; i<DlinhaB; i++){
+        for(j=0; j<DcolunaB; i++){
+            scanf("%d", &matrizB[i][j]);
+        }
+    }
+    for(i=0;i<DlinhaB; i++){
+        for(j=0; j<DcolunaB; j++){
+            printf("|%4d", matrizB[i][j]);
+        }
+        printf("|\n");
+    }
+    }
+    else{
+        printf("Nao e possivel realizar esta multiplicacao.\n");
+    }
 }
